6e5f7f2fe4ea3893af3b6aa4874dff4accd98eee
Musa Unal <umusasadik@gmail.com>
2022-12-01 20:49:20 +0100

    Limit max thread usage

--- scripts/setup/base_setup.sh
+++ scripts/setup/base_setup.sh
@@ -9,11 +9,11 @@ fi
 make -C ${PSP_DIR}/submodules/fake_work libfake
 
 # Setup RocksDB
-make -j -C ${PSP_DIR}/submodules/rocksdb static_lib
+make -j25 -C ${PSP_DIR}/submodules/rocksdb static_lib
 
 # Setup Perséphone
 mkdir ${PSP_DIR}/build && cd ${PSP_DIR}/build
 cmake -DCMAKE_BUILD_TYPE=Release -DDPDK_MELLANOX_SUPPORT=OFF ${PSP_DIR}
-make -j -C ${PSP_DIR}/build
+make -j25 -C ${PSP_DIR}/build
 
 # TODO: setup RocksDB database for the RocksDB worker
