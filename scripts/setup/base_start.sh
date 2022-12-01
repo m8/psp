78ff6df7828b6b89eeaf424b758688a8678e500a
Musa Unal <umusasadik@gmail.com>
2022-12-01 20:52:08 +0100

    Configs for s1

--- scripts/setup/base_start.sh
+++ scripts/setup/base_start.sh
@@ -18,8 +18,8 @@ sudo ${PSP_DIR}/scripts/setup/turbo.sh disable
 #TODO the following steps are only necessary for non bifurcated drivers.
 #TODO make the PCI ID configurable
 # Unbind the NIC from the kernel driver
-sudo ${PSP_DIR}/submodules/dpdk/usertools/dpdk-devbind.py --force -u 18:00.1
+sudo ${PSP_DIR}/submodules/dpdk/usertools/dpdk-devbind.py --force -u 83:00.1
 # Load uio to bypass the kernel and use the NIC. Need the module built for the kernel in use
 sudo modprobe uio
 sudo insmod ${PSP_DIR}/submodules/dpdk/x86_64-native-linuxapp-gcc/build/kernel/linux/igb_uio/igb_uio.ko
-sudo ${PSP_DIR}/submodules/dpdk/usertools/dpdk-devbind.py -b igb_uio 18:00.1
+sudo ${PSP_DIR}/submodules/dpdk/usertools/dpdk-devbind.py -b igb_uio 83:00.1
