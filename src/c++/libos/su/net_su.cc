919a2d38947b8614f66ee856937d4637f5598640
Musa Unal <umusasadik@gmail.com>
2022-12-03 11:40:59 +0100

    Change fake networker

--- src/c++/libos/su/net_su.cc
+++ src/c++/libos/su/net_su.cc
@@ -1,8 +1,11 @@
 #include <arpa/inet.h>
 #include <sys/stat.h>
 #include <psp/libos/su/NetSu.hh>
+#include <psp/dispatch.h>
 
 #define MAX_CLIENTS 64
+
+extern struct task_queue tskq[CFG_MAX_PORTS];
 
 namespace po = boost::program_options;
 
@@ -27,55 +30,42 @@ int NetWorker::work(int status, unsigned long payload) {
     // Dispatch enqueued requests
     
     PSP_OK(dpt.dispatch());
-     
 
-    // Check if we got packets from the network
-    if (udp_ctx->recv_packets() != EAGAIN) {
-        uint64_t cur_tsc = rdtscp(NULL);
-        //Process a batch of packets
-        size_t batch_dequeued = 0;
-        n_batchs_rcvd++;
-        while (udp_ctx->pop_head > udp_ctx->pop_tail and batch_dequeued < MAX_RX_BURST) {
-            unsigned long req = udp_ctx->inbound_queue[udp_ctx->pop_tail & (INBOUND_Q_LEN - 1)];
-            /*
-            if (unlikely(is_echo)) {
-                //PSP_OK(udp_ctx->free_mbuf(&sga));
-                if (unlikely(udp_ctx->push_head - udp_ctx->push_tail == OUTBOUND_Q_LEN)) {
-                    PSP_WARN("Outbound UDP queue full. Freeing mbuf " << req);
-                    PSP_OK(udp_ctx->free_mbuf(req));
-                } else {
-                    udp_ctx->pop_tail++;
-                    udp_ctx->outbound_queue[udp_ctx->push_head++ & (OUTBOUND_Q_LEN - 1)] = req;
-                }
-            } else {
-            */
-                int ret = dpt.enqueue(req, cur_tsc);
-                if (ret == EXFULL or ret == ENOENT) {
-                    // Free the request because we can't enqueue it
-                    PSP_OK(udp_ctx->free_mbuf(req));
-                    //break;
-                }
-                udp_ctx->pop_tail++;
-            //}
-            batch_dequeued++;
+    uint64_t cur_tsc = rdtscp(NULL);
+    //Process a batch of packets
+    size_t batch_dequeued = 0;
+    n_batchs_rcvd++;
+
+    while (udp_ctx->pop_head > udp_ctx->pop_tail and batch_dequeued < MAX_RX_BURST) {
+
+        unsigned long req = udp_ctx->inbound_queue[udp_ctx->pop_tail & (INBOUND_Q_LEN - 1)];
+        
+        int ret = 0;
+
+        int * cont = nullptr;
+        
+        tskq_enqueue_tail(&tskq[0], cont, (void *)req,
+                            0, PACKET, cur_tsc);      
+
+        printf("Enqueued packet\n");      
+        
+        if (ret == EXFULL or ret == ENOENT) {
+            // Free the request because we can't enqueue it
+            PSP_OK(udp_ctx->free_mbuf(req));
+            //break;
         }
-        //PSP_DEBUG("Net worker dequeued " << batch_dequeued << " requests");
-        n_rcvd += batch_dequeued;
-    }
-    /*
-    if (unlikely(is_echo)) {
-        PSP_OK(udp_ctx->send_packets());
-    }
-    */
 
+        udp_ctx->pop_tail++;
+        batch_dequeued++;
+    }
+    n_rcvd += batch_dequeued;
+    
     return 0;
 }
 
 int NetWorker::fake_work(int status)
 {
     uint64_t cur_tsc = rdtscp(NULL);
-    n_batchs_rcvd++;
-
     // create mbuf
     struct rte_mbuf *mbuf = rte_pktmbuf_alloc(udp_ctx->mbuf_pool);
     
@@ -85,15 +75,8 @@ int NetWorker::fake_work(int status)
         return ENOMEM;
     }
 
-    int ret = dpt.enqueue((unsigned long)mbuf, cur_tsc);
+    udp_ctx->inbound_queue[udp_ctx->pop_head++ & (INBOUND_Q_LEN - 1)] = (unsigned long) (void *) mbuf;
 
-    if (ret == EXFULL or ret == ENOENT)
-    {
-        printf("Net worker add backet failed\n");
-        // Free the request because we can't enqueue it
-        PSP_OK(udp_ctx->free_mbuf((unsigned long)mbuf));
-    }
-    udp_ctx->pop_tail++;
-
+    printf("Pushed packet\n");
     return 0;
 }
